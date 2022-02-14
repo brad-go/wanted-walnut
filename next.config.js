12e516d3bfd1b753e35b20f83e87bfbdcb4b7851
고동현 <dhjk35@naver.com>
2022-02-15 02:51:20 +0900

    :tada: start: 초기 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_URL: "localhost:3000",
+  },
+  webpack(config) {
+    // svg 불러오기
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    // 절대 경로 설정
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
