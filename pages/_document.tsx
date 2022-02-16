48853bb5d917638bfea0b89c258e28d517ee51d7
고동현 <dhjk35@naver.com>
2022-02-16 19:05:14 +0900

    :sparkles: feat: 메타 태그 작성

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,8 @@ import Document, {
   Main,
   NextScript,
   DocumentContext,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -37,6 +37,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="title" content="땅콩스쿨, 가장 가까운 문해력 유치원" />
+          <meta
+            name="description"
+            content="3만원으로 즐기는 매달 새로운 32권의 독서 커리큘럼. 평일 4~9시 캐릭터 선생님과 실시간 랜선라이브에 참여하세요!"
+          ></meta>
+          <meta property="og:title" content="땅콩스쿨"></meta>
+          <meta
+            property="og:url"
+            content="https://park-is-best-walnut.netlify.app/"
+          ></meta>
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
             rel="stylesheet"
