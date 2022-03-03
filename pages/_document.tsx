c601cbb11a128ce1ee51949ffe49b3c807983e4a
고동현 <dhjk35@naver.com>
2022-03-03 23:18:54 +0900

    :sparkles: feat: meta 태그 추가로 seo 확보 및 파비콘 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,11 +42,21 @@ class MyDocument extends Document {
             name="description"
             content="3만원으로 즐기는 매달 새로운 32권의 독서 커리큘럼. 평일 4~9시 캐릭터 선생님과 실시간 랜선라이브에 참여하세요!"
           ></meta>
-          <meta property="og:title" content="땅콩스쿨"></meta>
+          <meta property="og:title" content="땅콩스쿨" />
           <meta
             property="og:url"
             content="https://park-is-best-walnut.netlify.app/"
-          ></meta>
+          />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:image"
+            content="https://ddangkongschool.com/meta_img.png"
+          />
+          <meta
+            property="og:description"
+            content="캐릭터 선생님과 함께 실시간으로 즐기는 독서클래스"
+          />
+          <meta charSet="UTF-8" />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
             rel="stylesheet"
